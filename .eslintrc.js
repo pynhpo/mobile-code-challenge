99cd77bd71bfe2b02825f2eb7a3c40910e4e28d5
pynhpo <vothanhbinh0995@gmail.com>
2021-08-28 13:17:26 +0700

    Update configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,32 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
 };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    [
+      'module-resolver',
+      {
+        root: ['./src/'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@assets': './src/assets',
+          '@init': './src/init',
+          '@locales': './src/locales',
+          '@screens': './src/screens',
+          '@services': './src/services',
+          '@navigation': './src/navigation',
+          '@redux': './src/redux',
+          '@constants': './src/constants',
+        },
+      },
+    ],
+  ],
 };
